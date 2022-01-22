3a40a8cf842d6d067fd3f2b89bd09f19f5a1726d
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-22 22:45:00 +0300

    created other consumer layer

---
+++ Src/Consumer/OtherConsumer/Program.cs
@@ -0,0 +1,45 @@
+using Consumer.Consumers;
+using Consumer.Services;
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//MassTransit
+builder.Services.AddMassTransit(busConfigurator =>
+{
+    busConfigurator.AddConsumer<RegisterConsumer>();
+    busConfigurator.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+    {
+        cfg.ReceiveEndpoint("other-consumer", receieEndpointConfigurator =>
+        {
+            receieEndpointConfigurator.ConfigureConsumer<RegisterConsumer>(provider);
+        });
+    }));
+});
+
+// Hosted services
+builder.Services.AddHostedService<RabbitMqBackgroundService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Src/Consumer/OtherConsumer/Services/RabbitMqBackgroundService.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+
+namespace Consumer.Services
+{
+    public class RabbitMqBackgroundService : IHostedService
+    {
+        private readonly IBusControl _busControl;
+        private readonly ILogger<RabbitMqBackgroundService> _logger;
+
+        public RabbitMqBackgroundService(IBusControl busControl
+            , ILogger<RabbitMqBackgroundService> logger)
+        {
+            _busControl = busControl;
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Consumer background bus service started");
+
+            await _busControl.StartAsync(cancellationToken);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogError("Consumer background bus service stop!");
+
+            await _busControl.StopAsync(cancellationToken);
+        }
+    }
+}
