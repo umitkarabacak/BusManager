13ec5b09827205bb4a3420a88a6910e32f031beb
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-22 18:34:02 +0300

    first publisher configuration changed

--- Src/Publisher/PublisherA/Program.cs
+++ Src/Publisher/PublisherA/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//MassTransit
+builder.Services.AddMassTransit(busConfigurator =>
+{
+    busConfigurator.AddBus(provider => Bus.Factory.CreateUsingRabbitMq());
+});
 
 var app = builder.Build();
 
