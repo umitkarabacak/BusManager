da1ec06361d34995483fd57536beb050d183825d
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-01-22 18:37:59 +0300

    second publisher project configuration completed.

---
+++ Src/Publisher/PublisherB/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace PublisherB.Controllers
+{
+    public class RegisterViewModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+        private readonly IBus _bus;
+
+        public AuthController(ILogger<AuthController> logger
+            , IBus bus)
+        {
+            _logger = logger;
+            _bus = bus;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errorMessage = $"Error serialized item type is: {typeof(RegisterViewModel)} and current item: {JsonSerializer.Serialize(model)}";
+                _logger.LogError(errorMessage);
+
+                return BadRequest(ModelState);
+            }
+
+            await _bus.Publish(model);
+
+            return Ok();
+        }
+    }
+}
